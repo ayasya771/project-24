d76f17811f8eddc89125c753abc3f55ebeff82fd
ayasya771 <73102257+ayasya771@users.noreply.github.com>
2021-02-16 20:19:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var paper;
+
+function setup() {
+	createCanvas(900, 500);
+
+
+	engine = Engine.create();
+
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	World.add(world, packageBody);
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+	paper = new Paper();
+	ground = new Ground();
+	bin = new Dustbin();
+	Engine.run(engine);
+  
+}
+function keyPressed(){
+
+	if(keyCode == UP_ARROW){
+
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:30,y:-20})
+
+	}
+
+}
+
+
+
+function draw() {
+  rectMode(CENTER);
+  background("black");
+  ground.display();
+  paper.display();
+	bin.display();
+ 
+}
+
+
+
+
+
